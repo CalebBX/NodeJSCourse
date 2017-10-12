5098fc9ed46e0cfb03b737425390fdaea1c1d4e3
CalebBX <cbrocomp@gmail.com>
2017-10-12 15:45:11 -0700

    added error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,13 +14,18 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
     var encodedAddress = encodeURIComponent(argv.address);
-    console.log(encodedAddress);
 
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 },function(error, response, body){
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if(error){
+        console.log('Unable to connect to google servers.');
+    }else if(body.status === 'ZERO_RESULTS'){
+        console.log('Unable to find that address');
+    }else if(body.status === 'OK'){
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);  
+    }
 });
