6842cabc5003b66f503efec05e8756239ec4af4d
CalebBX <cbrocomp@gmail.com>
2017-10-16 23:34:22 -0700

    Changed app to use axios library and promises

---
+++ weather-app/app-promise.js
@@ -0,0 +1,40 @@
+const yargs = require('yargs');
+const axios = require('axios');
+
+const argv = yargs
+    .options({
+        address:{
+            demand: true,
+            alias: 'a',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+    
+var encodedAddress = encodeURIComponent(argv.address);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then(function(response){
+    if(response.data.status === 'ZERO_RESULTS'){
+        throw new Error('Unable to find that address.');
+    }
+
+    var lat = response.data.results[0].geometry.location.lat;
+    var lng = response.data.results[0].geometry.location.lng;
+    var weatherUrl = `https://api.darksky.net/forecast/1028ddecc5b06cad874786cc30cf1c8a/${lat},${lng}`;
+    console.log(response.data.results[0].formatted_address);
+    return axios.get(weatherUrl);
+}).then(function(response){
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}`);
+}).catch(function(e){
+    if(e.code === 'ENOTFOUND'){
+        console.log('Unable to connect to API servers.');
+    }else{
+        console.log(e.message);
+    }
+});
